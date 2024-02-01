9ec9894998db2b8397bc0efab24618b05c374c87
Or Hasson <hassonor@gmail.com>
2024-02-01 10:25:25 +0200

    add ch2 soltions ticpp vol_1

---
+++ TICPP_VOL1/ch_2/headers/CH2SolutionsTICPP.h
@@ -0,0 +1,18 @@
+//
+// Created by Or on 2/1/2024.
+//
+
+#ifndef CPP_PLAYGROUND_2_CH2SOLUTIONSTICPP_H
+#define CPP_PLAYGROUND_2_CH2SOLUTIONSTICPP_H
+
+
+#include <string>
+
+void countWordOccurrences(const std::string& filepath, const std::string& searchWord);
+
+int countWhiteSeperatedWords(const std::string& filename);
+
+void floatVector(const float& start, const float& end, const int& size);
+
+
+#endif //CPP_PLAYGROUND_2_CH2SOLUTIONSTICPP_H
