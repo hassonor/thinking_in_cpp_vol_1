cbef69c0709feb3f6625e4955ef168643b01b36e
Or Hasson <hassonor@gmail.com>
2024-02-08 20:19:37 +0200

    add pointer to memeber data example for ch11

---
+++ TICPP_VOL1/ch_11/headers/PointerToMemberData.h
@@ -0,0 +1,19 @@
+//
+// Created by Or on 2/8/2024.
+//
+
+#ifndef CPP_PLAYGROUND_2_POINTERTOMEMBERDATA_H
+#define CPP_PLAYGROUND_2_POINTERTOMEMBERDATA_H
+
+
+class Data {
+public:
+    int a, b, c;
+
+    void print() const;
+};
+
+void testDataClass();
+
+
+#endif //CPP_PLAYGROUND_2_POINTERTOMEMBERDATA_H
